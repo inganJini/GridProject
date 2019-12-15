a62edaf7fc23a50a39d410d03b3a9db4d032061f
Gapjin Yang <dkvowk@naver.com>
2019-12-15 23:16:11 +0900

    메인 그리드 수정

--- Perpect/diff/grid/views.py
+++ Perpect/diff/grid/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def grid(request):
-    return render(request, 'grid.html')
+    for_index = range(8)
+    return render(request, 'grid.html',{'for_index':for_index})
